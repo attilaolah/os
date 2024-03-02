98b5542b87c4a248828b64aac8885a91045bcceb
Attila Oláh <attilaolah@gmail.com>
2024-03-02 11:41:20 +0100

    Move home config to hosts dir.

--- flake.nix
+++ flake.nix
@@ -1,18 +1,21 @@
 {
-  description = "NixOS flakes.";
+  description = "NixOS flakes for attilaolah's personal computers.";
 
   inputs = {
     # renovate: datasource=github-releases depName=nixpkgs/nixos
     nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.workstation = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      format = "raw-efi";
-      modules = [
-        ./configuration.nix
-      ];
+  outputs = { self, nixpkgs, nixos-unstable, ... }@inputs: {
+    nixosConfigurations = {
+      home = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          (import ./hosts/home/configuration.nix)
+        ];
+        specialArgs = { inherit inputs; };
+      };
     };
   };
 }
--- hosts/home/configuration.nix
+++ hosts/home/configuration.nix
@@ -142,7 +142,12 @@
 
   sound.enable = true;
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    package = pkgs.nixUnstable;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
     git
