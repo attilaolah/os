856a724cd4554e88ef18b8dcacf45b724ff782a0
Attila Oláh <attilaolah@gmail.com>
2024-03-02 11:20:40 +0100

    Move settings to configuration.nix.

---
+++ configuration.nix
@@ -0,0 +1,163 @@
+{ lib, pkgs, ... }:
+
+{
+  system.stateVersion = "23.11";
+  time.timeZone = "Europe/Zurich";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  boot = {
+    # Enable TTY0 logging.
+    # https://github.com/nix-community/nixos-generators?tab=readme-ov-file#format-specific-notes
+    kernelParams = [ "console=tty0" ];
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiSupport = true;
+        enableCryptodisk = true;
+      };
+    };
+    initrd = {
+      kernelModules = [ "amdgpu" "kvm-intel" ];
+      luks.devices = {
+        crypta = {
+          # 2T NVMe SSD.
+          device = "/dev/disk/by-uuid/8efccfcb-8c61-4844-89c9-3dbc1a900de4";
+          allowDiscards = true;
+          preLVM = true;
+        };
+        cryptb = {
+          # 2T NVMe SSD.
+          device = "/dev/disk/by-uuid/a4d6ae21-1535-42d0-b5a9-c249d1db71d4";
+          allowDiscards = true;
+          preLVM = true;
+        };
+      };
+      services.lvm.enable = true;
+      # Start SSH during boot, to allow remote unlocking of LUKS volumes.
+      network.ssh.enable = true;
+    };
+  };
+
+  fileSystems = {
+    "/" = {
+      # LVM volume over crypta + cryptb.
+      device = "/dev/nixvg/root";
+      fsType = "ext4";
+    };
+    "boot" = {
+      # TMP; TODO: Move to 2Gi USB ESP partition.
+      device = "/dev/disk/by-uuid/EC50-CA49";
+      fsType = "vfat";
+    };
+  };
+
+  console = {
+    earlySetup = true;
+    # TODO: Remove, no need.
+    # keyMap = "dvorak-programmer";
+    useXkbConfig = true; # use xkb.options
+  };
+
+  hardware = {
+    # CPU: Intel Xeon E5-2666 v3
+    cpu.intel.updateMicrocode = true;
+    opengl.extraPackages = with pkgs; [
+      rocmPackages.clr.icd
+    ];
+    pulseaudio.enable = true;
+  };
+
+  systemd.tmpfiles.rules = [
+    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+  ];
+
+  security = {
+    polkit.enable = true;
+    sudo.execWheelOnly = true;
+  };
+
+  users = {
+    mutableUsers = false;
+    defaultUserShell = pkgs.fish;
+
+    # Generate a password using:
+    # nix-shell --run "mkpasswd -m SHA-512 -s" -p mkpasswd
+    users = {
+      root.initialHashedPassword = "$6$chB8XnZbQno6p5Zp$IE6xp/WGQYYwZGgAkQE9juhofD./R2ITPryZftBellWbeRKUtGBjBGOER6g9Qym.oDiMpkPc7OFOE4fxAV.fd/";
+
+      ao = {
+        isNormalUser = true;
+        initialHashedPassword = "$6$vIhSgctj5NiIagWv$OJQuVZnf8diIJQQHG83WxCaEr3gczTNyiQJGDCU1gqpgrA7.gnjaIJ19KjLJbyAIBWxhqd51E/6hgmHeziJIe0";
+        group = "ao";
+        extraGroups = [ "wheel" ];  # for sudo
+        openssh.authorizedKeys.keys = [
+          # https://github.com/attilaolah.keys
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiR17IcWh8l3OxxKSt+ODrUMLU98ZoJ+XvcR17iX9/P"
+        ];
+      };
+    };
+
+    groups.ao = {};
+  };
+
+  networking = {
+    # TODO: useDHCP = lib.mkForce true;
+    hostName = "home";
+    search = [ "dorn.haus" ];
+    firewall.allowedTCPPorts = [ 22 8080 ];
+
+    networkmanager.enable = true;
+    nftables.enable = true;
+  };
+
+  services = {
+    dbus.enable = true;
+
+    openssh = {
+      enable = true;
+      startWhenNeeded = true;  # make it socket-activated
+    };
+
+    # Only used on the virtual console.
+    xserver.xkb = {
+      layout = "us";
+      variant = "dvp";
+      options = "caps:escape,compose:ralt,keypad:atm,kpdl:semi,numpad:shift3";
+    };
+  };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+    ];
+  };
+
+  sound.enable = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+    wget
+  ];
+
+  programs = {
+    fish.enable = true;
+    hyprland.enable = true;
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+  };
+}
