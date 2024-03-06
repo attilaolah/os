abe52886b891f81b2383a0a91dd5e4cf3fd04711
Attila Oláh <attilaolah@gmail.com>
2024-03-06 17:24:36 +0100

    Add XDG dirs.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -22,13 +22,23 @@
     homeDirectory = "/home/ao";
     stateVersion = "23.11";
 
-    sessionVariables = {
+    sessionVariables = with config.home; {
       SHELL = "${pkgs.fish}/bin/fish";
       VISUAL = "nvim";
 
       # Development environment:
-      GOPATH = "${config.home.homeDirectory}/dev/go";
-      RUSTUP_HOME = "${config.home.homeDirectory}/dev/rustup";
+      GOPATH = "${homeDirectory}/dev/go";
+      RUSTUP_HOME = "${homeDirectory}/dev/rustup";
+
+      # XDG dirs:
+      XDG_DESKTOP_DIR = homeDirectory;
+      XDG_DOWNLOAD_DIR = "${homeDirectory}/dl";
+      XDG_PICTURES_DIR = "${homeDirectory}/photos";
+      XDG_PUBLICSHARE_DIR = "${homeDirectory}/p2p";
+      XDG_VIDEOS_DIR = "${homeDirectory}/videos";
+      # XDG_DOCUMENTS_DIR not set
+      # XDG_MUSIC_DIR not set
+      # XDG_TEMPLATES_DIR not set
     };
   };
 
