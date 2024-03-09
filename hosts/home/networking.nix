b2e9338859b94ce27cd9af5f24789f6f4141eb9d
Attila Oláh <attilaolah@gmail.com>
2024-03-09 15:02:25 +0100

    Update Wireguard subnet.

--- hosts/home/networking.nix
+++ hosts/home/networking.nix
@@ -24,18 +24,12 @@ in {
 
     wireguard.interfaces = {
       dh8 = {
-        ips = [
-          "fd10:8::2/64" # jh: fd10:8::1
-          "10.8.0.2/16" # jh: 10.8.0.1
-        ];
+        ips = ["fd10:4::2/64"]; # jh: fd10:4::1
         privateKeyFile = "/etc/wireguard/dh8.home.key";
         peers = [
           {
             publicKey = "dQNahUFr2rm4uRqPIE4ZwwGt9WXcoUtkVOmApM8AYU8=";
-            allowedIPs = [
-              "::/0" # IPv6: all traffic
-              "10.8.0.0/16" # IPv4: 16-bit network only
-            ];
+            allowedIPs = ["::/0"]; # IPv6: all traffic
             endpoint = "192.168.4.1:45340";
             persistentKeepalive = 24;
           }
