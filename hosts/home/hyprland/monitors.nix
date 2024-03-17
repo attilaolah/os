d1cf8215b465648702a0fffe40a0fe888b2ad1c5
Attila Oláh <attilaolah@gmail.com>
2024-03-17 18:23:00 +0100

    Swap left & right monitors.

--- hosts/home/hyprland/monitors.nix
+++ hosts/home/hyprland/monitors.nix
@@ -1,22 +1,22 @@
 {lib, ...}: let
   # Monitor layout:
   # ╔═══╗         ╔═══╗
-  # ║ L ║╔═══════╗║ D ║
+  # ║ D ║╔═══════╗║ L ║
   # ║ E ║║ S A M ║║ E ║
-  # ║ N ║╚═══════╝║ L ║
+  # ║ L ║╚═══════╝║ N ║
   # ╚═══╝         ╚═══╝
-  M1 = "Lenovo Group Limited P27h-20 V906V9HY";
+  M1 = "Dell Inc. DELL P2720DC 81WTK01K1SFS";
   M2 = "Samsung Electric Company U32J59x HNMW200264";
-  M3 = "Dell Inc. DELL P2720DC 81WTK01K1SFS";
+  M3 = "Lenovo Group Limited P27h-20 V906V9HY";
 in rec {
   "$M1" = "desc:${M1}";
   "$M2" = "desc:${M2}";
   "$M3" = "desc:${M3}";
 
   monitor = [
-    "$M1, 2560x1440@60, 0x0,      1, transform, 1"
+    "$M1, 2560x1440@60, 0x0,      1, transform, 3"
     "$M2, 3840x2160@60, 1440x200, 1"
-    "$M3, 2560x1440@60, 5280x0,   1, transform, 3"
+    "$M3, 2560x1440@60, 5280x0,   1, transform, 1"
   ];
 
   workspace = [
