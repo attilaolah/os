e8a811433bb3d91c67b32bcddc4e042cfa18b984
Attila Oláh <attilaolah@gmail.com>
2024-04-03 22:58:53 +0200

    Bump GPG agent cache TTL to 8h.

--- home-manager/services/gpg_agent.nix
+++ home-manager/services/gpg_agent.nix
@@ -2,7 +2,7 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    defaultCacheTtl = 60 * 60 * 2; # 2h in secs
+    defaultCacheTtl = 60 * 60 * 8; # 8h in secs
     pinentryPackage = with pkgs; lib.mkForce pinentry-curses;
     sshKeys = ["2CCBD7C415983C9B20762124B39B41EC96B810DA"];
   };
