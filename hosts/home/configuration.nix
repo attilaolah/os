b37f5f02063b7e285be458ed83fbad2e2738b207
Attila Oláh <attilaolah@gmail.com>
2024-04-21 14:17:46 +0200

    Remove Grafana + friends, Nginx. (#28)

--- hosts/home/configuration.nix
+++ hosts/home/configuration.nix
@@ -10,15 +10,10 @@
     ./services/blueman.nix
     ./services/davfs2.nix
     ./services/dbus.nix
-    ./services/grafana.nix
     ./services/greetd.nix
     ./services/gvfs.nix
-    ./services/loki.nix
-    ./services/nginx.nix
     ./services/openssh.nix
     ./services/pipewire.nix
-    ./services/prometheus.nix
-    ./services/promtail.nix
     ./services/tailscale.nix
     ./services/teleport.nix
     ./services/xserver/xkb.nix
