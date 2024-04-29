31efe70dfa64c2864bfea78d1311b7289976098f
Attila Oláh <attilaolah@gmail.com>
2024-04-29 13:47:28 +0200

    Upse TTY pinentry instead of the NCurses one.

--- home-manager/services/gpg_agent.nix
+++ home-manager/services/gpg_agent.nix
@@ -3,7 +3,7 @@
     enable = true;
     enableSshSupport = true;
     defaultCacheTtl = 60 * 60 * 8; # 8h in secs
-    pinentryPackage = with pkgs; lib.mkForce pinentry-curses;
+    pinentryPackage = with pkgs; lib.mkForce pinentry-tty;
     sshKeys = ["2CCBD7C415983C9B20762124B39B41EC96B810DA"];
   };
 }
