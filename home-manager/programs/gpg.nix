2fb6cdb0d017009c2897a29061add051abc82a21
Attila Oláh <attila@dorn.haus>
2024-09-23 20:34:21 +0200

    GnuPG config (#98)
    * Add GnuPG key & trustdb to home-manager.
    
    * Trust GitHub's signing key.
    
    * Remove GPG public key from the repo.
    
    It is already hosted at https://github.com/attilaolah.gpg anyway, so we can reference it from there.

--- home-manager/programs/gpg.nix
+++ home-manager/programs/gpg.nix
@@ -1,6 +1,24 @@
-{
+{pkgs, ...}: {
   programs.gpg = {
     enable = true;
+    mutableKeys = false;
+    mutableTrust = false;
+    publicKeys = [
+      {
+        source = pkgs.fetchurl {
+          url = "https://github.com/attilaolah.gpg";
+          hash = "sha256-0xBHzPfbfx8buL3kH4EjNDaetZ5REWTMZQe4X1qNVBE=";
+        };
+        trust = "ultimate";
+      }
+      {
+        source = pkgs.fetchurl {
+          url = "https://github.com/web-flow.gpg";
+          hash = "sha256-bor2h/YM8/QDFRyPsbJuleb55CTKYMyPN4e9RGaj74Q=";
+        };
+        trust = "full";
+      }
+    ];
     settings.default-key = "07E6C0643FD142C3";
   };
 }
