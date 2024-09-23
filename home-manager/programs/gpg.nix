d81ffa0eb0e006709601aa9889553ac4051edba5
Attila Oláh <attila.olah@netstal.com>
2024-09-23 20:24:09 +0200

    Trust GitHub's signing key.

--- home-manager/programs/gpg.nix
+++ home-manager/programs/gpg.nix
@@ -8,6 +8,13 @@
         source = ./gpg.asc;
         trust = "ultimate";
       }
+      {
+        source = builtins.fetchurl {
+          url = "https://github.com/web-flow.gpg";
+          sha256 = "sha256:117gldk49gc76y7wqq6a4kjgkrlmdsrb33qw2l1z9wqcys3zd2kf";
+        };
+        trust = "full";
+      }
     ];
     settings.default-key = "07E6C0643FD142C3";
   };
