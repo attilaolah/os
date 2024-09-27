0038236c893dbe68153d0fdf842ad5d0a49ce32f
Attila Oláh <attila@dorn.haus>
2024-09-27 19:18:33 +0200

    Pass along any git log args.

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -21,7 +21,8 @@
             --pretty=format:"%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s" \
             --date=short \
             --decorate \
-            --graph
+            --graph \
+            "$@"
         }
         log
       '';
