044cf2550a21b4122207e62d3a04c83ba3a484da
Attila Oláh <attila@dorn.haus>
2024-10-01 22:20:46 +0200

    Move XDG config files to a better place.

--- home-manager/file.nix
+++ home-manager/file.nix
@@ -1,93 +1,13 @@
-{
-  config,
-  desktop,
-  lib,
-  pkgs,
-  ...
-}: let
-  desktopAttrs = attrs: lib.attrsets.optionalAttrs desktop attrs;
-  foot-catppuccin-mocha = pkgs.stdenv.mkDerivation {
-    name = "foot-catppuccin-mocha";
-    # TODO: renovate
-    src = pkgs.fetchFromGitHub {
-      owner = "catppuccin";
-      repo = "foot";
-      rev = "ee5549af72ab78520ac2aa1c671bf5c2d347c8ca";
-      sha256 = "sha256-3hK9klXwdHhprG2wUMt7nBfbL1mb/gl+k/MtJUuY000=";
+{lib, ...}: {
+  home.file = {
+    ".sops.yaml".text = lib.generators.toYAML {} {
+      creation_rules = [
+        {pgp = "BF2E475974D388E0E30C960407E6C0643FD142C3";}
+      ];
     };
-    installPhase = ''
-      cp --recursive catppuccin-mocha.ini $out
+    ".ssh/config".text = ''
+      # Force GPG-agent's pinentry to use the current tmux pane.
+      Match host * exec "gpg-connect-agent UPDATESTARTUPTTY /bye"
     '';
   };
-in {
-  home.file =
-    {
-      ".sops.yaml".text = lib.generators.toYAML {} {
-        creation_rules = [
-          {pgp = "BF2E475974D388E0E30C960407E6C0643FD142C3";}
-        ];
-      };
-      ".ssh/config".text = ''
-        # Force GPG-agent's pinentry to use the current tmux pane.
-        Match host * exec "gpg-connect-agent UPDATESTARTUPTTY /bye"
-      '';
-      # TODO: Use xdg.configFile!
-      ".config/nvim/init.lua".source = ./.config/nvim/init.lua;
-      ".config/nvim/lua/chadrc.lua".source = ./.config/nvim/lua/chadrc.lua;
-      ".config/nvim/lua/configs/conform.lua".source = ./.config/nvim/lua/configs/conform.lua;
-      ".config/nvim/lua/configs/lazy.lua".source = ./.config/nvim/lua/configs/lazy.lua;
-      ".config/nvim/lua/configs/lspconfig.lua".source = ./.config/nvim/lua/configs/lspconfig.lua;
-      ".config/nvim/lua/mappings.lua".source = ./.config/nvim/lua/mappings.lua;
-      ".config/nvim/lua/options.lua".source = ./.config/nvim/lua/options.lua;
-      ".config/nvim/lua/plugins/init.lua".source = ./.config/nvim/lua/plugins/init.lua;
-    }
-    // desktopAttrs (let
-      brightnessctl = lib.getExe pkgs.brightnessctl;
-      hyprctl = lib.getExe' pkgs.hyprland "hyprctl";
-      hyprlock = lib.getExe pkgs.hyprlock;
-      loginctl = lib.getExe' pkgs.systemd "loginctl";
-      pidof = lib.getExe' pkgs.procps "pidof";
-    in {
-      # TODO: Use xdg.configFile!
-      ".config/foot/foot.ini".text =
-        (lib.generators.toINI {} {
-          main = {
-            font = "monospace:size=16";
-            font-size-adjustment = 2;
-          };
-        })
-        + builtins.readFile foot-catppuccin-mocha;
-
-      ".config/hypr/hypridle.conf".text = ''
-        general {
-          lock_cmd = ${pidof} ${hyprlock} || ${hyprlock}
-          before_sleep_cmd = ${loginctl} lock-session
-          after_sleep_cmd = ${hyprctl} dispatch dpms on
-        }
-
-        listener {
-          timeout = ${toString (60 * 2)}
-          on-timeout = ${brightnessctl} --save set 5%
-          on-resume = ${brightnessctl} --restore
-        }
-        listener {
-          timeout = ${toString (60 * 20)}
-          on-timeout = ${hyprctl} dispatch dpms off
-          on-resume = ${hyprctl} dispatch dpms on
-        }
-      '';
-      #  listener {
-      #    timeout = ${toString (60 * 20 + 20)}
-      #    on-timeout = ${loginctl} lock-session
-      #  }
-
-      ".config/davfs.conf".text = ''
-        secrets ${config.home.homeDirectory}/.config/davfs.secrets
-      '';
-
-      # Wallpaper:
-      ".config/wallpapers/alpeli-1020m.jpg".source = ./.config/wallpapers/alpeli-1020m.jpg;
-
-      ".config/wofi/style.css".source = ./.config/wofi/style.css;
-    });
 }
