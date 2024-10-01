95df800e3411170e73cf9a0caf46bdda2730c67f
Attila Oláh <attila@dorn.haus>
2024-10-01 22:28:23 +0200

    Cleanup (#117)
    * Remove obsolete jh config.
    
    * Move XDG config files to a better place.
    
    * Fix tmux binding.

--- home-manager/xdg/config_file.nix
+++ home-manager/xdg/config_file.nix
@@ -1,5 +1,82 @@
 {
-  xdg.configFile = {
-    "hypr/hyprlock.conf".source = ./hypr/hyprlock.conf;
+  config,
+  desktop,
+  lib,
+  pkgs,
+  ...
+}: let
+  desktopAttrs = attrs: lib.attrsets.optionalAttrs desktop attrs;
+  foot-catppuccin-mocha = pkgs.stdenv.mkDerivation {
+    name = "foot-catppuccin-mocha";
+    # TODO: renovate
+    src = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "foot";
+      rev = "ee5549af72ab78520ac2aa1c671bf5c2d347c8ca";
+      sha256 = "sha256-3hK9klXwdHhprG2wUMt7nBfbL1mb/gl+k/MtJUuY000=";
+    };
+    installPhase = ''
+      cp --recursive catppuccin-mocha.ini $out
+    '';
   };
+in {
+  xdg.configFile =
+    {
+      "hypr/hyprlock.conf".source = ./hypr/hyprlock.conf;
+      "nvim/init.lua".source = ./nvim/init.lua;
+      "nvim/lua/chadrc.lua".source = ./nvim/lua/chadrc.lua;
+      "nvim/lua/configs/conform.lua".source = ./nvim/lua/configs/conform.lua;
+      "nvim/lua/configs/lazy.lua".source = ./nvim/lua/configs/lazy.lua;
+      "nvim/lua/configs/lspconfig.lua".source = ./nvim/lua/configs/lspconfig.lua;
+      "nvim/lua/mappings.lua".source = ./nvim/lua/mappings.lua;
+      "nvim/lua/options.lua".source = ./nvim/lua/options.lua;
+      "nvim/lua/plugins/init.lua".source = ./nvim/lua/plugins/init.lua;
+    }
+    // desktopAttrs (let
+      brightnessctl = lib.getExe pkgs.brightnessctl;
+      hyprctl = lib.getExe' pkgs.hyprland "hyprctl";
+      hyprlock = lib.getExe pkgs.hyprlock;
+      loginctl = lib.getExe' pkgs.systemd "loginctl";
+      pidof = lib.getExe' pkgs.procps "pidof";
+    in {
+      # TODO: Use xdg.configFile!
+      "foot/foot.ini".text =
+        (lib.generators.toINI {} {
+          main = {
+            font = "monospace:size=16";
+            font-size-adjustment = 2;
+          };
+        })
+        + builtins.readFile foot-catppuccin-mocha;
+
+      "hypr/hypridle.conf".text = ''
+        general {
+          lock_cmd = ${pidof} ${hyprlock} || ${hyprlock}
+          before_sleep_cmd = ${loginctl} lock-session
+          after_sleep_cmd = ${hyprctl} dispatch dpms on
+        }
+
+        listener {
+          timeout = ${toString (60 * 2)}
+          on-timeout = ${brightnessctl} --save set 5%
+          on-resume = ${brightnessctl} --restore
+        }
+        listener {
+          timeout = ${toString (60 * 20)}
+          on-timeout = ${hyprctl} dispatch dpms off
+          on-resume = ${hyprctl} dispatch dpms on
+        }
+      '';
+      # TODO: re-enable!
+      # listener {
+      #   timeout = ${toString (60 * 20 + 20)}
+      #   on-timeout = ${loginctl} lock-session
+      # }
+
+      "davfs.conf".text = ''
+        secrets ${config.home.homeDirectory}/.config/davfs.secrets
+      '';
+
+      "wofi/style.css".source = ./wofi/style.css;
+    });
 }
--- hosts/jh/README.md
+++
@@ -1,6 +0,0 @@
-# SSH + WireGuard jump host
-
-Currently a Raspberry Pi model B+ running Alpine linux. Used to access the
-[DH8 cluster][1] from outside the firewall, and from IPv4-only networks.
-
-[1]: https://gitlab.com/dornhaus/ops
