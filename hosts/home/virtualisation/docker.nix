0075fbcca5f51633db2c3c65ac28b1c19b49c5c5
Attila Oláh <attila@dorn.haus>
2024-11-13 14:08:06 +0100

    Simplify Docker config.

--- hosts/home/virtualisation/docker.nix
+++ hosts/home/virtualisation/docker.nix
@@ -4,12 +4,10 @@
 
     enableOnBoot = false; # use socket activation
 
-    daemon = {
-      settings = {
-        ipv6 = true;
-        fixed-cidr-v6 = "fd10:6::/64";
-        features.containerd-snapshotter = true;
-      };
+    daemon.settings = {
+      ipv6 = true;
+      fixed-cidr-v6 = "fd10:6::/64";
+      features.containerd-snapshotter = true;
     };
   };
 }
