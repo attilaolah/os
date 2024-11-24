a68d715c658b617a810a4944f8103fc95526990a
Attila Oláh <attila@dorn.haus>
2024-11-24 11:58:18 +0100

    Remove obsolete rocm tmpfiles rules.

--- hosts/home/systemd.nix
+++ hosts/home/systemd.nix
@@ -1,16 +1,9 @@
 {pkgs, ...}: {
-  systemd = {
-    tmpfiles.rules = [
-      # AMD ROCm HIP location, as expected by some 3rd party apps.
-      "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
-    ];
-
-    services = {
-      # Fix nixos-rebuild hanging, until this issue is resolved:
-      # https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-1658731959
-      NetworkManager-wait-online.serviceConfig = {
-        ExecStart = ["" "${pkgs.networkmanager}/bin/nm-online -q"];
-      };
+  systemd.services = {
+    # Fix nixos-rebuild hanging, until this issue is resolved:
+    # https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-1658731959
+    NetworkManager-wait-online.serviceConfig = {
+      ExecStart = ["" "${pkgs.networkmanager}/bin/nm-online -q"];
     };
   };
 }
