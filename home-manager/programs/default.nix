2b3bbb20cea4cc2f7f8755c9695867eecfc48850
Attila Oláh <attila@dorn.haus>
2025-05-09 17:05:29 +0200

    Install some CLI tools.

--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -1,12 +1,16 @@
 {
   imports = [
+    ./command-not-found.nix
     ./dircolors.nix
     ./direnv.nix
     ./fd.nix
     ./fish
     ./git.nix
     ./gpg.nix
+    ./poetry.nix
     ./rbw.nix
+    ./tealdeer.nix
     ./tmux.nix
+    ./uv.nix
   ];
 }
---
+++ home-manager/programs/poetry.nix
@@ -0,0 +1,3 @@
+{
+  programs.poetry.enable = true;
+}
