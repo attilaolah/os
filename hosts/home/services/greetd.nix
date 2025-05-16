56d746d1a11558a660587a4e2a1cd00348312418
Attila Oláh <attila@dorn.haus>
2025-05-16 16:29:28 +0200

    Fix tuigreet command.

--- hosts/home/services/greetd.nix
+++ hosts/home/services/greetd.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.lib.getExe pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command = "${pkgs.lib.getExe pkgs.greetd.tuigreet} --time --cmd Hyprland";
         user = "greeter";
       };
     };
