3262914d370e073e284c723f4889a7ce712b003b
Attila Oláh <attila@dorn.haus>
2025-07-18 15:08:26 +0000

    feat: add laptop keygrip

--- home-manager/services/gpg_agent.nix
+++ home-manager/services/gpg_agent.nix
@@ -4,6 +4,9 @@
     enableSshSupport = true;
     defaultCacheTtl = 8 * 60 * 60; # 8h in secs
     pinentry.package = with pkgs; lib.mkForce pinentry-tty;
-    sshKeys = ["2CCBD7C415983C9B20762124B39B41EC96B810DA"];
+    sshKeys = [
+      "2CCBD7C415983C9B20762124B39B41EC96B810DA" # workstation
+      "41AAF08280AC45DD018D66EE8499FC8E211706D7" # laptop
+    ];
   };
 }
