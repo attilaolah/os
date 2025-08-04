a5b32166525bfd472bf047ce0f46874bbffc574f
Attila Oláh <attila@dorn.haus>
2025-08-04 18:38:59 +0200

    feat: set GPG_AGENT_INFO

--- home-manager/programs/fish/interactive_shell_init.nix
+++ home-manager/programs/fish/interactive_shell_init.nix
@@ -5,10 +5,12 @@
 }: let
   any-nix-shell = lib.getExe pkgs.any-nix-shell;
   direnv = lib.getExe pkgs.direnv;
+  gpgconf = lib.getExe' pkgs.gnupg "gpgconf";
   zoxide = lib.getExe pkgs.zoxide;
 in {
   programs.fish.interactiveShellInit = ''
     set --universal fish_greeting
+    set --global --export GPG_AGENT_INFO (${gpgconf} --list-dirs agent-socket)
 
     source ${pkgs.fzf}/share/fish/vendor_functions.d/fzf_key_bindings.fish
     source ${pkgs.fzf}/share/fish/vendor_conf.d/load-fzf-key-bindings.fish
