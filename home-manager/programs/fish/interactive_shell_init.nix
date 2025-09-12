eb051b2a3e2f505cdae909a275a7ca3d3887efda
Attila Oláh <attila@dorn.haus>
2025-09-12 11:28:36 +0200

    fix: fzf fish key bindings

--- home-manager/programs/fish/interactive_shell_init.nix
+++ home-manager/programs/fish/interactive_shell_init.nix
@@ -12,8 +12,7 @@ in {
     set --universal fish_greeting
     set --global --export GPG_AGENT_INFO (${gpgconf} --list-dirs agent-socket)
 
-    source ${pkgs.fzf}/share/fish/vendor_functions.d/fzf_key_bindings.fish
-    source ${pkgs.fzf}/share/fish/vendor_conf.d/load-fzf-key-bindings.fish
+    source ${pkgs.fzf}/share/fzf/key-bindings.fish
 
     ${any-nix-shell} fish | source
     ${direnv} hook fish | source
