c5f941bd9a3c9fe28828d62569106fa06a8d8d1b
Attila Oláh <attila@dorn.haus>
2025-10-20 10:54:59 +0000

    feat: use the same tmux navigation keys in neovim + tmux

--- home-manager/xdg/nvim/lua/mappings.lua
+++ home-manager/xdg/nvim/lua/mappings.lua
@@ -1,6 +1,13 @@
 require "nvchad.mappings"
 
 local map = vim.keymap.set
+local opts = { silent = true, noremap = true }
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Ctrl + Alt/Meta + Arrow keys
+map("n", "<C-M-Left>", "<cmd>TmuxNavigateLeft<CR>", opts)
+map("n", "<C-M-Down>", "<cmd>TmuxNavigateDown<CR>", opts)
+map("n", "<C-M-Up>", "<cmd>TmuxNavigateUp<CR>", opts)
+map("n", "<C-M-Right>", "<cmd>TmuxNavigateRight<CR>", opts)
