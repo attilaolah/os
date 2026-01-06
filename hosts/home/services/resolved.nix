d9c8517910300dc119f451a8d16fafb288acc98f
Attila Oláh <attila@dorn.haus>
2026-01-06 09:02:47 +0100

    misc: use one.one.one.one for tls verification

--- hosts/home/services/resolved.nix
+++ hosts/home/services/resolved.nix
@@ -6,7 +6,7 @@
     # Point to Google + Cloudflare with the hostname for certificate verification.
     extraConfig = ''
       DNS=8.8.8.8#dns.google 8.8.4.4#dns.google
-      FallbackDNS=1.1.1.1#cloudflare-dns.com 1.0.0.1#cloudflare-dns.com
+      FallbackDNS=1.1.1.1#one.one.one.one 1.0.0.1#one.one.one.one
     '';
   };
 }
