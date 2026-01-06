b40fe6ddb0b83774ef8ecf6e1aa42aa25eddca2b
Attila Oláh <attila@dorn.haus>
2026-01-06 08:42:02 +0100

    refactor: inline service config

--- hosts/home/systemd.nix
+++ hosts/home/systemd.nix
@@ -19,12 +19,8 @@
       })
       nodes;
 
-    services = {
-      # Fix nixos-rebuild hanging, until this issue is resolved:
-      # https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-1658731959
-      NetworkManager-wait-online.serviceConfig = {
-        ExecStart = ["" "${pkgs.networkmanager}/bin/nm-online -q"];
-      };
-    };
+    # Fix nixos-rebuild hanging, until this issue is resolved:
+    # https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-1658731959
+    services.NetworkManager-wait-online.serviceConfig.ExecStart = ["" "${pkgs.networkmanager}/bin/nm-online -q"];
   };
 }
