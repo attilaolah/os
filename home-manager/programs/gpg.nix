af7f682b8d0bec01c5824f85d559dc4ffd312688
Attila Oláh <attila@dorn.haus>
2026-04-21 22:24:02 +0200

    feat: set primary gpg email

--- home-manager/programs/gpg.nix
+++ home-manager/programs/gpg.nix
@@ -7,7 +7,7 @@
       {
         source = pkgs.fetchurl {
           url = "https://github.com/attilaolah.gpg";
-          hash = "sha256-ibRC4EuKw6dZByY/kgi0RdeGvQybLJfdHWZzr1TabfQ=";
+          hash = "sha256-uSo7GvgnEK97thekH6uio8WTbHlqI+Tg/kT1OEreNF0=";
         };
         trust = "ultimate";
       }
