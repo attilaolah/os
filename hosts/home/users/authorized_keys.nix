326e6f50c38f0da41f1e5a57ccd14863e6201ebc
Attila Oláh <attila@dorn.haus>
2026-04-21 22:17:52 +0200

    feat: hard-code ssh keys

--- hosts/home/users/authorized_keys.nix
+++ hosts/home/users/authorized_keys.nix
@@ -1,12 +1,8 @@
-{
-  pkgs,
-  user,
-  ...
-}: let
-  inherit (pkgs) lib;
-in {
-  users.users."${user.username}".openssh.authorizedKeys.keys = lib.splitString "\n" (lib.removeSuffix "\n" (builtins.readFile (pkgs.fetchurl {
-    url = "https://github.com/attilaolah.keys";
-    hash = "sha256-Nw/XiCnJ+KmHK1YFO3x9MF+GPfCWWyPmy//go4UKg/M=";
-  })));
+{user, ...}: {
+  users.users."${user.username}".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiR17IcWh8l3OxxKSt+ODrUMLU98ZoJ+XvcR17iX9/P" # Workstation
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP0Y/37XG4iBs4hHLI88dQQJhtVVal69GRF7HpHT+60J" # Work laptop
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCK3wit0j7rkD4q7DTAij1Swsk6zzCiJZyH6hthB+7Hou49XkEPbt3Obs6541x7LLD4v5XDo0CSm5QGSr2GgqJI=" # MacOS (Secretive)
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBC+mtV6yrvijOAmvsstRCYsUSbc8ZI3Np7qY2rWuACNaAnLSRhu5qbL/1EzZgcRFbMKaqRYLy8Tq56PDjck2MTo=" # Android (Terminus)
+  ];
 }
