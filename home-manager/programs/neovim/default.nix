f0371546a5b20e08dc2975729384e6df54db9c23
Attila Oláh <attila@dorn.haus>
2026-04-21 18:17:34 +0200

    feat: neovim: use imports & defaults

--- home-manager/programs/neovim/default.nix
+++ home-manager/programs/neovim/default.nix
@@ -1,25 +1,24 @@
 {pkgs, ...}: {
-  programs.neovim =
-    # Defaults defined in the hosts file:
-    (import ../../../hosts/home/programs/neovim.nix).programs.neovim
-    # Overrides:
-    // {
-      vimdiffAlias = true;
-      extraPackages = with pkgs; [
-        # Language servers:
-        kotlin-language-server
-        lua-language-server
-        vscode-langservers-extracted
-        yaml-language-server
+  imports = [
+    ../../../hosts/home/programs/neovim.nix
+  ];
+  programs.neovim = {
+    vimdiffAlias = true;
+    extraPackages = with pkgs; [
+      # Language servers:
+      kotlin-language-server
+      lua-language-server
+      vscode-langservers-extracted
+      yaml-language-server
 
-        # Unmaintained, maybe pick it up.
-        # ansible-language-server
-      ];
-      initLua = builtins.readFile ./init.lua;
+      # Unmaintained, maybe pick it up.
+      # ansible-language-server
+    ];
+    initLua = builtins.readFile ./init.lua;
 
-      # Enable additional language support in the version installed by home-manager.
-      withNodeJs = true;
-      withPython3 = true;
-      withRuby = true;
-    };
+    # Enable additional language support in the version installed by home-manager.
+    withNodeJs = true;
+    withPython3 = true;
+    withRuby = true;
+  };
 }
--- hosts/home/programs/neovim.nix
+++ hosts/home/programs/neovim.nix
@@ -1,12 +1,14 @@
-{
-  programs.neovim = {
+{lib, ...}: {
+  programs.neovim = let
+    off = lib.mkDefault false;
+  in {
     enable = true;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
 
     # NixOS 26.05 defaults:
-    withPython3 = false;
-    withRuby = false;
+    withPython3 = off;
+    withRuby = off;
   };
 }
