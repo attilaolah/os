f03a35e71b90e120c1447c42c5633c69e499c469
Attila Oláh <attila@dorn.haus>
2026-04-28 13:51:45 +0200

    chore(nitpick): validate github-tags tuple defensively

--- overlays/lib/fetch_from_github_tuple.nix
+++ overlays/lib/fetch_from_github_tuple.nix
@@ -4,13 +4,27 @@
 }: {
   github-tags,
   hash,
-  rev ? builtins.elemAt github-tags 1,
+  rev ? null,
 }: let
-  inherit (builtins) elemAt;
+  inherit (builtins) elemAt isList isString length throw;
   githubRepo = lib.splitString "/" (elemAt github-tags 0);
+  resolvedRev =
+    if rev == null
+    then elemAt github-tags 1
+    else rev;
 in
-  fetchFromGitHub {
-    inherit hash rev;
-    owner = elemAt githubRepo 0;
-    repo = elemAt githubRepo 1;
-  }
+  assert (isList github-tags && length github-tags >= 2)
+  || throw "github-tags must be a list of at least two strings: owner and repo";
+  assert (
+    isString (elemAt github-tags 0)
+    && elemAt github-tags 0 != ""
+    && isString (elemAt github-tags 1)
+    && elemAt github-tags 1 != ""
+  )
+  || throw "github-tags must contain non-empty string values for the first two elements";
+    fetchFromGitHub {
+      inherit hash;
+      rev = resolvedRev;
+      owner = elemAt githubRepo 0;
+      repo = elemAt githubRepo 1;
+    }
