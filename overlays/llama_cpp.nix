2a0bdfd268f574c584c49b045559c9968fd77086
Attila Oláh <attila@dorn.haus>
2026-04-28 10:20:28 +0200

    refactor: renovate lines without repeating metadata in comments

--- overlays/llama_cpp.nix
+++ overlays/llama_cpp.nix
@@ -1,15 +1,19 @@
 final: prev: let
-  # renovate: datasource=github-tags depName=ggml-org/llama.cpp extractVersion=^b(?<version>.*)$
-  version = "8884";
+  inherit (builtins) elemAt;
+
+  github-tags = ["ggml-org/llama.cpp" "8884"]; # extractVersion=^b(?<version>.*)$
   hash = "sha256-pQvoAIcoYkCI2z93YQP737Zuj3PzpgPGlR5HezRneSE=";
   npmDepsHash = "sha256-RAFtsbBGBjteCt5yXhrmHL39rIDJMCFBETgzId2eRRk=";
+
+  version = elemAt github-tags 1;
+  githubRepo = prev.lib.splitString (elemAt github-tags 0) "/";
 in {
   llama-cpp = prev.llama-cpp.overrideAttrs (_old: {
     inherit version npmDepsHash;
     src = prev.fetchFromGitHub {
       inherit hash;
-      owner = "ggml-org";
-      repo = "llama.cpp";
+      owner = elemAt githubRepo 0;
+      repo = elemAt githubRepo 1;
       rev = "b${version}";
     };
   });
