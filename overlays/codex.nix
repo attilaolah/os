774debe19d7c077cb239ec07a0ba45020131123f
Attila Oláh <attila@dorn.haus>
2026-05-08 08:20:44 +0200

    chore(codex): update hashes

--- overlays/codex.nix
+++ overlays/codex.nix
@@ -5,8 +5,8 @@ final: prev: let
   };
 
   github-tags = ["openai/codex" "0.129.0"]; # extractVersion=^rust-v(?<version>.*)$
-  hash = "sha256-v2W0eslPOPHxHX76+bnkE/f4y+MnQuopeOoAC5X16TA=";
-  cargoHash = "sha256-3NQ4UCfBpANhyoJJatd8m31cEugsd42Ye2BXuzlKC0c=";
+  hash = "sha256-IvBeuDiP1vMzCGVxi4CWKcYajP+IkNIyHmREO5gz2dU=";
+  cargoHash = "sha256-H4iqXfcT9mvU6O4j5TybfyKEpx522iaggS68AC1jbU8=";
 
   version = elemAt github-tags 1;
 in {
