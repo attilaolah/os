76c47ba1ad819c3ac59ef788dae40cb42b020fd0
Attila Oláh <attila@dorn.haus>
2026-07-09 15:58:43 +0200

    Update keyboard remappings, fix Homebrew cleanup (#322)
    * cleanup: uninstall alt-tab now that it became garbage
    
    * cleanup: uninstall programmer-dvorak, since dvp compose is already installed
    
    * feat: switch homebrew back to zap, as the system profile has no homebrew
    
    * feat: remap option+tab to cmd+tab
    
    * misc: reword karabiner rule description
    
    * feat: add convenience ghostty key bindings for copy/paste
    
    * feat: add chrome tab shortcut remaps
    
    * refactor: karabiner config
    
    * refactor: karabiner config
    
    * feat: add more chrome shortcut remaps
    
    * refactor: karabiner config for dvp layout
    
    * feat: remap chrome find + new window shortcuts
    
    * fix: dvp f, n; add new private window shortcut
    
    * refactor: karabiner config
    
    * refactor: chrome shortcuts
    
    * refactor: simplify chrome shortcuts
    
    * feat: add text selection + clipboard shortcuts

--- home_manager/xdg/config_file/karabiner/profiles/managed.nix
+++ home_manager/xdg/config_file/karabiner/profiles/managed.nix
@@ -1,17 +1,20 @@
-{
+let
+  inherit (builtins) attrValues mapAttrs;
+
+  # Keyboard identifiers.
+  # Any keyboard (in practice, this applies to the built-in keyboard).
+  keyboard.is_keyboard = true;
+  # Das Keyboard:
+  das_keyboard = {
+    product_id = 320;
+    vendor_id = 9456;
+  };
+in {
   name = "Managed";
   selected = true;
   virtual_hid_keyboard.keyboard_type_v2 = "iso";
 
   devices = let
-    # Keyboard identifiers.
-    # Any keyboard (in practice, this applies to the built-in keyboard).
-    keyboard.is_keyboard = true;
-    # Das Keyboard:
-    das_keyboard = {
-      product_id = 320;
-      vendor_id = 9456;
-    };
     # Keyboard-specific remapping.
     remap = key_code: {
       simple_modifications = [
@@ -27,8 +30,8 @@
     ({identifiers = keyboard // das_keyboard;} // (remap "right_option"))
   ];
 
-  simple_modifications = builtins.attrValues (
-    builtins.mapAttrs (from: to: {
+  simple_modifications = attrValues (
+    mapAttrs (from: to: {
       from.key_code = from;
       to = [{key_code = to;}];
     }) {
@@ -38,6 +41,32 @@
   );
 
   complex_modifications.rules = let
+    inherit (builtins) attrNames concatMap elemAt head isAttrs replaceStrings;
+
+    dvp = {
+      a = "a";
+      c = "i";
+      f = "y";
+      h = "j";
+      j = "c";
+      l = "p";
+      n = "l";
+      p = "r";
+      s = "semicolon";
+      t = "k";
+      u = "f";
+      v = "period";
+      w = "comma";
+      x = "b";
+      y = "t";
+    };
+    das_keyboard_only.conditions = [
+      {
+        type = "device_if";
+        identifiers = [das_keyboard];
+      }
+    ];
+
     any = key_code: mandatory: from key_code mandatory ["any"];
     from = key_code: mandatory: optional: {
       from = {
@@ -53,15 +82,66 @@
     };
     to = key_code: modifiers: {to = [{inherit key_code modifiers;}];};
     basic = manipulators: map (mod: mod // {type = "basic";}) manipulators;
+    shortcuts = concatMap (
+      spec: let
+        from_modifiers = elemAt spec 0;
+        to_modifiers = elemAt spec 1;
+        keys = elemAt spec 2;
+      in
+        map (
+          key: let
+            mapping =
+              if isAttrs key
+              then key
+              else {${key} = key;};
+            from_key = head (attrNames mapping);
+            to_key = mapping.${from_key};
+          in
+            (any dvp.${from_key} from_modifiers) // (to dvp.${to_key} to_modifiers)
+        )
+        keys
+    );
+    frontmost = bundle_identifier: manipulators:
+      map (mod:
+        mod
+        // {
+          conditions = [
+            {
+              type = "frontmost_application_if";
+              bundle_identifiers = ["^${replaceStrings ["."] ["\\."] bundle_identifier}$"];
+            }
+          ];
+        })
+      manipulators;
   in [
     {
-      description = "Windows/Linux-style text navigation shortcuts";
+      description = "App and window switching shortcuts";
+      manipulators = basic [
+        ((any "tab" ["option" "shift"]) // (to "tab" ["command" "shift"]))
+        ((any "tab" ["option"]) // (to "tab" ["command"]))
+        ((any "grave_accent_and_tilde" ["command" "shift"]) // (to "equal_sign" ["command" "option" "shift"]))
+        ((any "grave_accent_and_tilde" ["command"]) // (to "equal_sign" ["command" "option"]))
+        ((any "grave_accent_and_tilde" ["left_option"]) // (to "equal_sign" ["command" "option"]) // das_keyboard_only)
+      ];
+    }
+    {
+      description = "Text navigation shortcuts";
       manipulators = basic [
         ((any "left_arrow" ["control" "shift"]) // (to "left_arrow" ["option" "shift"]))
         ((any "right_arrow" ["control" "shift"]) // (to "right_arrow" ["option" "shift"]))
         ((any "left_arrow" ["control"]) // (to "left_arrow" ["option"]))
         ((any "right_arrow" ["control"]) // (to "right_arrow" ["option"]))
       ];
+    }
+    {
+      description = "Chrome shortcuts";
+      manipulators = basic (frontmost "com.google.Chrome" (shortcuts [
+        [["control" "shift"] ["command" "shift"] ["t" "n"]]
+        [["control" "shift"] ["command" "option"] ["j"]]
+        [["control"] ["command" "shift"] ["j"]]
+        [["control"] ["command" "option"] ["u"]]
+        [["control"] ["command"] ["a" "c" "f" "l" "n" "p" "s" "t" "v" "w" "x" {h = "y";}]]
+      ]));
     }
   ];
 }
--- hosts/work/homebrew.nix
+++ hosts/work/homebrew.nix
@@ -3,8 +3,9 @@
     enable = true;
 
     onActivation = {
-      # All Homebrew packages must be declared below.
-      cleanup = "check";
+      # Uninstall packages not present in this config.
+      # This is intentional. All Homebrew packages must be declared below.
+      cleanup = "zap";
       # Do a full update & upgrade on activation.
       # This is a convenient way to remember to upgrade these packages.
       autoUpdate = true;
@@ -18,11 +19,9 @@
 
     # GUI applications:
     casks = [
-      "alt-tab"
       "font-jetbrains-mono-nerd-font"
       "ghostty"
       "karabiner-elements"
-      "programmer-dvorak"
       "secretive"
       "tigervnc"
     ];
