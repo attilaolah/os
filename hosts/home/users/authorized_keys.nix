50644df6ceb7f39c0d6fc81ef8914d3be7a79a1f
Attila Oláh <attila@dorn.haus>
2026-07-16 22:22:25 +0200

    chore: update ssh keys (#350)

--- hosts/home/users/authorized_keys.nix
+++ hosts/home/users/authorized_keys.nix
@@ -1,8 +1,10 @@
 {user, ...}: {
   users.users."${user.username}".openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiR17IcWh8l3OxxKSt+ODrUMLU98ZoJ+XvcR17iX9/P" # Workstation
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP0Y/37XG4iBs4hHLI88dQQJhtVVal69GRF7HpHT+60J" # Work laptop
-    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDv26UnORlvl+WMrucBQDSwZHzrOjtNvyCThj1PXtI28XhxFRomp/tmIG4Hy4qnOgkQDmqYHYmFRhz/7N/Bq+SM=" # MacOS (Secretive)
-    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBC+mtV6yrvijOAmvsstRCYsUSbc8ZI3Np7qY2rWuACNaAnLSRhu5qbL/1EzZgcRFbMKaqRYLy8Tq56PDjck2MTo=" # Android (Terminus)
+    # Workstation
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiR17IcWh8l3OxxKSt+ODrUMLU98ZoJ+XvcR17iX9/P"
+    # Secretive (MacBook)
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJjvtnPSFVm3JL2oRLk9njHRiWBiFmsdbLmJNhcG4wu9UN+OFGJTM4pJhNys4rr0i+6xC/+9GTwNRw5McYmpW8c="
+    # Terminus (Android)
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBC+mtV6yrvijOAmvsstRCYsUSbc8ZI3Np7qY2rWuACNaAnLSRhu5qbL/1EzZgcRFbMKaqRYLy8Tq56PDjck2MTo="
   ];
 }
