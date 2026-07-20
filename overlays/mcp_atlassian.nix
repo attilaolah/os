ec73dca1141d0e663c6e903cae95edc495bc651a
Attila Oláh <attila@dorn.haus>
2026-07-20 18:31:53 +0200

    fix: remove patches already upstream

--- overlays/mcp_atlassian.nix
+++ overlays/mcp_atlassian.nix
@@ -18,23 +18,6 @@ in {
       rev = "v${version}";
     };
 
-    patches = [
-      (prev.fetchpatch {
-        url = "https://github.com/sooperset/mcp-atlassian/commit/b089ed8b3b7ccdde207ce3ab2b7ef88e412a6f53.patch";
-        hash = "sha256-Jyyb6iKJT84Ee8DtNswwsH3wsNSokXHtkwrjz30eQuo=";
-      })
-      (prev.fetchpatch {
-        url = "https://github.com/sooperset/mcp-atlassian/commit/4822b754c93d562ada627fa84a985703e3ba9e46.patch";
-        hash = "sha256-wlIeVinKPAdFGJxYF15pi145ZrTeJ7oCkzUifLjxyyo=";
-        excludes = ["uv.lock"];
-      })
-      (prev.fetchpatch {
-        url = "https://github.com/attilaolah/mcp-atlassian/commit/e41849b9b5294d61e67546fe9005caaeebae078b.patch";
-        hash = "sha256-FZeigTPnb8sdloClCoP0DPFJxlwA1J1qa5ofs72EtsM=";
-        excludes = ["uv.lock"];
-      })
-    ];
-
     build-system = with py; [
       hatchling
       uv-dynamic-versioning
