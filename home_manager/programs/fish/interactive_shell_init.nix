a99ed1bd9b3d545a3b7e9135b48ac05637fd83f7
Attila Oláh <attila@dorn.haus>
2026-08-23 20:48:20 +0200

    fix re-bind fzf to ctrl+f to free up ctrl+t for bosun

--- home_manager/programs/fish/interactive_shell_init.nix
+++ home_manager/programs/fish/interactive_shell_init.nix
@@ -7,9 +7,15 @@
     set fish_greeting
     # TODO: Remove this line once the atuin home-manager module is fixed for fish > 4.3.0
     set -g fish_key_bindings fish_default_key_bindings
+
     set --global --export GPG_AGENT_INFO (
       ${lib.getExe' pkgs.gnupg "gpgconf"} --list-dirs agent-socket
     )
+
+    # Re-bind fzf's default find-file from Ctrl+T to Ctrl+F.
+    # This frees up Ctrl+T, passing it through to be used as the new-tab shortcut in bosun.
+    bind --erase \ct
+    bind \cf fzf-file-widget
 
     source ${pkgs.catppuccin-fzf}
     set --export FZF_DEFAULT_OPTS "--style=full $FZF_DEFAULT_OPTS"
