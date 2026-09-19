410400b78849277138db5c92e67f7c07f885abc1
Attila Oláh <attila@dorn.haus>
2026-09-19 21:07:45 +0200

    chore(opencode): update hash overrides for darwin

--- overlays/opencode.nix
+++ overlays/opencode.nix
@@ -7,13 +7,13 @@ _final: prev: {
           if prev.stdenv.hostPlatform.isLinux
           then "sha256-8mOzCscBAuogG4tm8CroqjTX5E5yzCvTobbhKvxQP0U="
           else if prev.stdenv.hostPlatform.isDarwin
-          then "sha256-wAea8+jajnMDxZ6XJL+Hsrf0621hwtBtWyD1+dS45dE="
+          then "sha256-YY5A/zxLPONvgnI+DZlzcD2K5Q9PIw4F2YbDxKbT4UU="
           else nodeModulesAttrs.outputHash;
         outputHash =
           if prev.stdenv.hostPlatform.isLinux
           then "sha256-4vGsS3fXeIYkMbPoUiVkaABgjZmiIF2IG/VwxY58vcw="
           else if prev.stdenv.hostPlatform.isDarwin
-          then "sha256-BlpLVz+IPdoxXrE76K5mm74k9ZJbMxu08jT4tb8Lv2s="
+          then "sha256-+Bon3V9z60OdjlD4edi8I4Hua8z7mDaY6pyAXOpEutg="
           else nodeModulesAttrs.outputHash;
       in
         assert nodeModulesAttrs.outputHash == brokenHash;
