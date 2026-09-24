9d3c5ecaa50e6364b48fe8437c9da760094b9875
Attila Oláh <attila@dorn.haus>
2026-09-24 13:08:33 +0200

    chore(opencode): update expected source hashes

--- overlays/opencode.nix
+++ overlays/opencode.nix
@@ -5,15 +5,15 @@ _final: prev: {
       node_modules = oldAttrs.node_modules.overrideAttrs (nodeModulesAttrs: let
         brokenHash =
           if prev.stdenv.hostPlatform.isLinux
-          then "sha256-8mOzCscBAuogG4tm8CroqjTX5E5yzCvTobbhKvxQP0U="
+          then "sha256-+Clo0VPDdruHSoBNvV/wKAM8iR6HJPtB00oa8yl9ujU="
           else if prev.stdenv.hostPlatform.isDarwin
-          then "sha256-YY5A/zxLPONvgnI+DZlzcD2K5Q9PIw4F2YbDxKbT4UU="
+          then "sha256-pThjoD6baddQ6biy7k1ByXwGwLAeWe/+w0tcYmt1uWs="
           else nodeModulesAttrs.outputHash;
         outputHash =
           if prev.stdenv.hostPlatform.isLinux
-          then "sha256-4vGsS3fXeIYkMbPoUiVkaABgjZmiIF2IG/VwxY58vcw="
+          then prev.lib.fakeHash
           else if prev.stdenv.hostPlatform.isDarwin
-          then "sha256-+Bon3V9z60OdjlD4edi8I4Hua8z7mDaY6pyAXOpEutg="
+          then prev.lib.fakeHash
           else nodeModulesAttrs.outputHash;
       in
         assert nodeModulesAttrs.outputHash == brokenHash;
